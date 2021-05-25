19733f0f08a0dcec9949159891f9addc8d55ef01
tom <hdhen88@gmail.com>
2021-05-25 11:28:56 +0800

    进行了修改

---
+++ src/main/java/com/bjpowernode/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.bjpowernode.git;
+
+/**
+ * @author daceag
+ * @date 2021/5/25 11:14
+ * @description
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}

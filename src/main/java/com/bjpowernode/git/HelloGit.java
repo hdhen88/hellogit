31142545f4e52d72e555af0d0e0601f4d3468ac2
tom <hdhen88@gmail.com>
2021-05-25 11:39:33 +0800

    创建分支

--- src/main/java/com/bjpowernode/git/HelloGit.java
+++ src/main/java/com/bjpowernode/git/HelloGit.java
@@ -8,5 +8,6 @@ package com.bjpowernode.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("创建分支");
     }
 }
